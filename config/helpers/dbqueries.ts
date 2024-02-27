8539cb3681a17c9a181247a7e00f8058d1566075
Ethan Quarry <dev.ethanquarry@gmail.com>
2024-02-27 17:21:44 +0000

    Add: Question Image rendering successful

--- config/helpers/dbqueries.ts
+++ config/helpers/dbqueries.ts
@@ -12,3 +12,19 @@ export const getProfileImg = async (id: string | undefined) => {
     }
     else return "https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F251659%2Fprofile&psig=AOvVaw22Qjh-Mmyqu9kXJqqGhCBg&ust=1708956814294000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLDavrDWxoQDFQAAAAAdAAAAABAE"
 }
+
+export const getQuestions = async (subject: string) => {
+    const questions = await db.questions.findMany({
+        where: {
+            subject: subject
+        },
+        include: {
+            questionSections: true,
+            markingSchemeSections: true
+        }
+    })
+    if (questions) {
+        return questions
+    }
+    else return []
+}
--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
     experimental: {
         instrumentationHook: true, 
     },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'blob-static.studyclix.ie',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
     webpack: (config) => {
 		config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
 		return config;

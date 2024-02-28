36c5fbeba2129da1f8f6f5d6dfbe0c5ad5a27e4d
Ethan Quarry <dev.ethanquarry@gmail.com>
2024-02-28 22:17:48 +0000

    Add: Multi-section question rendering added

--- app/(application)/leaving-cert/maths/[slug]/page.tsx
+++ app/(application)/leaving-cert/maths/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { maths} from "@/config/subjects";
+import { maths } from "@/config/subjects";
 import { getQuestions } from "@/config/helpers/dbqueries";
 import Image from 'next/image'
 
@@ -11,9 +11,70 @@ export default async function Page({ params }: { params: { slug: string } }) {
             return (
                 <div>
                     <h1>{item.name}</h1>
-                    <h1>hello</h1>
                     {questions.map((question, index) => {
-                        return (
+                        if (question.questionSections.length === 0) return (<div key={index} >No question sections</div>)
+                        else if (question.questionSections.length == 2) return (
+                            <div key={index} >
+                                <h2>{question.fullName}</h2>
+
+                                <div className="flex flex-col space-y-5">
+                                    <Image
+                                        src={question.questionSections[0].imageUrl}
+                                        alt="Question Sections"
+                                        width={500}
+                                        height={500}
+                                        placeholder="blur"
+                                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAOXRFWHRTb2Z0d2FyZQBNYXRwbG90bGliIHZlcnNpb24zLjQuMywgaHR0cHM6Ly9tYXRwbG90bGliLm9yZy/MnkTPAAAACXBIWXMAAB7CAAAewgFu0HU+AAEAAElEQVR4nOzdeXhUZf7/8e9"
+                                    />
+
+                                    <Image
+                                        src={question.questionSections[1].imageUrl}
+                                        alt="Question Sections"
+                                        width={500}
+                                        height={500}
+                                        placeholder="blur"
+                                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAOXRFWHRTb2Z0d2FyZQBNYXRwbG90bGliIHZlcnNpb24zLjQuMywgaHR0cHM6Ly9tYXRwbG90bGliLm9yZy/MnkTPAAAACXBIWXMAAB7CAAAewgFu0HU+AAEAAElEQVR4nOzdeXhUZf7/8e9"
+                                    />
+                                </div>
+                            </div>
+                        )
+
+                        else if (question.questionSections.length == 3) return (
+                            <div key={index} >
+                                <h2>{question.fullName}</h2>
+
+                                <div className="flex flex-col space-y-5">
+                                    <Image
+                                        src={question.questionSections[0].imageUrl}
+                                        alt="Question Sections"
+                                        width={500}
+                                        height={500}
+                                        placeholder="blur"
+                                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAOXRFWHRTb2Z0d2FyZQBNYXRwbG90bGliIHZlcnNpb24zLjQuMywgaHR0cHM6Ly9tYXRwbG90bGliLm9yZy/MnkTPAAAACXBIWXMAAB7CAAAewgFu0HU+AAEAAElEQVR4nOzdeXhUZf7/8e9"
+                                    />
+
+                                    <Image
+                                        src={question.questionSections[1].imageUrl}
+                                        alt="Question Sections"
+                                        width={500}
+                                        height={500}
+                                        placeholder="blur"
+                                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAOXRFWHRTb2Z0d2FyZQBNYXRwbG90bGliIHZlcnNpb24zLjQuMywgaHR0cHM6Ly9tYXRwbG90bGliLm9yZy/MnkTPAAAACXBIWXMAAB7CAAAewgFu0HU+AAEAAElEQVR4nOzdeXhUZf7/8e9"
+                                    />
+
+                                    <Image
+                                        src={question.questionSections[2].imageUrl}
+                                        alt="Question Sections"
+                                        width={500}
+                                        height={500}
+                                        placeholder="blur"
+                                        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAOXRFWHRTb2Z0d2FyZQBNYXRwbG90bGliIHZlcnNpb24zLjQuMywgaHR0cHM6Ly9tYXRwbG90bGliLm9yZy/MnkTPAAAACXBIWXMAAB7CAAAewgFu0HU+AAEAAElEQVR4nOzdeXhUZf7/8e9"
+                                    />
+                                </div>
+                            </div>
+                        )
+
+                        else return (
                             <div key={index} >
                                 <h2>{question.fullName}</h2>
                                 <Image
@@ -21,16 +82,18 @@ export default async function Page({ params }: { params: { slug: string } }) {
                                     alt="Question Sections"
                                     width={500}
                                     height={500}
-                                  />  
+                                    placeholder="blur"
+                                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAOXRFWHRTb2Z0d2FyZQBNYXRwbG90bGliIHZlcnNpb24zLjQuMywgaHR0cHM6Ly9tYXRwbG90bGliLm9yZy/MnkTPAAAACXBIWXMAAB7CAAAewgFu0HU+AAEAAElEQVR4nOzdeXhUZf7/8e9"
+                                />
                             </div>
                         )
                     })}
                 </div>
             )
+        }
     }
-}
     return (
         <div>Oops</div>
     )
-  }
+}
 
